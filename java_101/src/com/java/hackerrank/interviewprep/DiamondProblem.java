bb74f069eb7daa8e77530af241c5d0cbd2b106e7
asifjafar <Aspect001>
2019-04-08 14:46:58 +0530

    #2 - Java Demo project successfully created.

--- java_101/src/com/java/hackerrank/interviewprep/DiamondProblem.java
+++ java_101/src/com/java/hackerrank/interviewprep/DiamondProblem.java
@@ -1,5 +1,7 @@
 package com.java.hackerrank.interviewprep;
 
+import java.util.*;
+
 public class DiamondProblem implements C {
 
     @Override
@@ -8,12 +10,31 @@ public class DiamondProblem implements C {
     }
 
     public static void main(String args[]){
-        B demoB= new DiamondProblem();
-        A demoA = new DiamondProblem();
-        C demoC = new DiamondProblem();
-        demoA.display("A");
-        demoB.display("B");
-        demoC.display("C");
+        try {
+            B demoB = new DiamondProblem();
+            A demoA = new DiamondProblem();
+            C demoC = new DiamondProblem();
+            demoA.display("A");
+            demoB.display("B");
+            demoC.display("C");
+            String ef = "dfs";
+            ef.toCharArray();
+           int u=  1/0;
+            ef.intern();
+            List<Integer> list = new ArrayList<>();
+
+            Collections.sort(null, new Comparator<DiamondProblem>() {
+                @Override
+                public int compare(DiamondProblem o1, DiamondProblem o2) {
+                    return 0;
+                }
+            });
+
+        }catch (Exception e){
+
+        }finally {
+
+        }
     }
 }
 
